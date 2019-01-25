73f5d68e92862aadda546690cb072cf33b112ac5
Håkan Kilström <hakan.kilstrom@outlook.com>
2019-01-25 10:19:45 +0100

    Add project files.

---
+++ Garage20MvcCore22/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Garage20MvcCore22.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
